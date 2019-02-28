2a5442994841bf1c08f3a06530ed3e145366a978
Siva Sivasubramoniam Jayaram <sivajrm@gmail.com>
2019-02-27 18:32:11 -0800

    Added spaces

--- src/main/java/com/app/katchup/Meeting/model/Meeting.java
+++ src/main/java/com/app/katchup/Meeting/model/Meeting.java
@@ -18,31 +18,38 @@ public class Meeting {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private String meetingId;
+    
     @GeneratedValue(strategy = GenerationType.AUTO)
     private  String password;
+    
     @NotNull(message = MeetingConstants.dateTimeNullMessage)
     @NotEmpty
     private LocalDateTime startDateTime;
+    
     @NotNull(message = MeetingConstants.dateTimeNullMessage)
     @NotEmpty
     private LocalDateTime endDateTime;
+    
     @NotNull(message = MeetingConstants.inviteListNullMessage)
     List<String> inviteList;
+    
     @NotNull(message = MeetingConstants.venueNullMessage)
     @NotEmpty
     String venue;
+    
     @NotNull(message = MeetingConstants.responseNullMessage)
     @NotEmpty
     private Decision response;
-    @NotNull(message = MeetingConstants.durationNullMessage)
-    @NotEmpty
-    int duration;
+    
     @NotNull(message = UserConstants.userNameNullMessage)
     @Size(min = UserConstants.userNameMinLength, max = UserConstants.userNameMaxLength)
     private String host;
+    
     @NotNull(message = MeetingConstants.pollNullMessage)
     private Decision poll;
+    
     int seats;
+    
     @NotEmpty
     private Status status;
 
