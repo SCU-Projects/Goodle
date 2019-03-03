15a17fdd359328882f819346e3357dab51901bcc
SuryaChandrasekaran <csurya2460@gmail.com>
2019-03-02 18:06:43 -0800

    minor code changes to the calendar service and checking >0 and ==0 conditions.

--- src/main/java/com/app/katchup/DistributedCalendar/CalendarController.java
+++ src/main/java/com/app/katchup/DistributedCalendar/CalendarController.java
@@ -3,7 +3,6 @@ package com.app.katchup.DistributedCalendar;
 import com.app.katchup.DistributedCalendar.model.DistributedCalendar;
 import com.app.katchup.DistributedCalendar.model.Event;
 import com.app.katchup.Meeting.MeetingService;
-import com.app.katchup.Meeting.model.Meeting;
 import com.app.katchup.MeetingResponse.model.MeetingID;
 import com.app.katchup.Users.UserService;
 import org.apache.logging.log4j.LogManager;
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/calendar")
@@ -37,36 +35,16 @@ public class CalendarController {
     public ResponseEntity<DistributedCalendar> showUserEvents(@PathVariable String userName, HttpServletRequest request) {
         if (!(userService.isCredentialsMatched(userName, request.getHeader("password"))))
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        List<MeetingID> meetingIds = calendarService.getAcceptedMeetingIds(userName);
 
-        else {
-            List<MeetingID> meetingIds = calendarService.getAcceptedMeetingIds(userName);
-            List<Meeting> meetingDetailsList = new ArrayList<>();
+        if (meetingIds.isEmpty()) {
+            logger.info("there are no meetings accepted by user, so returns an empty event list");
             DistributedCalendar distributedCalendar = new DistributedCalendar();
             List<Event> eventList = new ArrayList<Event>();
-
-            if (meetingIds.isEmpty()) {
-                if (meetingIds.size() == 0)
-                    distributedCalendar.setEventList(eventList);
-            } else {
-                List<String> meetingIdsList = meetingIds.stream().map(meetingID -> meetingID.getMeetingId()).collect(Collectors.toList());
-                meetingDetailsList = meetingService.getMeetingDetailsForMeetingIds(meetingIdsList);
-                eventList = meetingDetailsList.stream()
-                        .map(meeting -> {
-                            Event event = new Event();
-                            event.setEndDateTime(meeting.getEndDateTime());
-                            event.setHost(meeting.getHost());
-                            event.setMeetingId(meeting.getMeetingId());
-                            event.setStartDateTime(meeting.getStartDateTime());
-                            event.setSubject(meeting.getSubject());
-                            event.setVenue(meeting.getVenue());
-                            return event;
-                        })
-                        .collect(Collectors.toList());
-                logger.info("all accepted events added to a new distributed calendar object");
-                distributedCalendar.setEventList(eventList);
-            }
+            distributedCalendar.setEventList(eventList);
             return new ResponseEntity<>(distributedCalendar, HttpStatus.OK);
-
+        } else {
+            return new ResponseEntity<>(calendarService.addAcceptedEventsToDistributedCalendar(meetingIds), HttpStatus.OK);
         }
     }
 }
