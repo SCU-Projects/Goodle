f6c76a2b931c06a25237b915868aac48932f8f9a
Surya Chandrasekaran <csurya2460@gmail.com>
2019-03-14 02:30:59 -0700

    deleted target folders from kafka consumer modules.

--- producer/src/main/java/com/app/katchup/Meeting/MeetingConstants.java
+++
@@ -1,9 +0,0 @@
-package com.app.katchup.Meeting;
-
-public class MeetingConstants {
-
-    public static final String dateTimeNullMessage = "Date/Time cannot be null";
-    public static final String inviteListNullMessage = "To list cannot be null";
-    public static final String venueNullMessage = "Venue cannot be null";
-    public static final String subjectNullMessage = "Meeting subject cannot be null";
-}
--- producer/src/main/java/com/app/katchup/Meeting/model/Status.java
+++
@@ -1,8 +0,0 @@
-package com.app.katchup.Meeting.model;
-
-public enum Status {
-    OPEN,
-    CLOSED,
-    UPDATE,
-    DELETED
-}
